de41e1fe1de936d2b85196b96e447086b58f3eb9
TunarN <tunarzn@code.edu.az>
2022-08-07 14:48:23 +0400

    Task1

---
+++ array metod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace array_metod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int num = 145;
+            Console.WriteLine(NumLength(num));
+        }
+        static int NumLength(int num)
+        {
+            num = 145;
+            int count = 0;
+            while (num>0)
+            {
+                num /= 10;
+                count++;
+                   
+            }
+            return count;
+        }
+        
+    }
+}
